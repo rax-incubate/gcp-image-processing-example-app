e63f56b10394b9873ac39f5c2a2ff07228b527cd
srirajan <sriram.rajan@gmail.com>
2023-01-17 17:41:46 +0000

    some formating changes

--- extract-text/main.py
+++ extract-text/main.py
@@ -1,5 +1,7 @@
 import functions_framework
 import os
+import json
+import uuid
 from google.cloud import storage
 from google.api_core.client_options import ClientOptions
 from google.cloud import documentai
@@ -9,6 +11,11 @@ from google.cloud import pubsub_v1
 @functions_framework.cloud_event
 # receive new image events from GCS
 def new_image_file(cloud_event):
+    eid = uuid.uuid4().hex
+    debugx = False
+    if os.environ.get('DEBUGX') == "1":
+        debugx = True
+
     data = cloud_event.data
 
     event_id = cloud_event["id"]
@@ -20,16 +27,21 @@ def new_image_file(cloud_event):
     timeCreated = data["timeCreated"]
     updated = data["updated"]
 
-    print(f"Debug: Event ID: {event_id}")
-    print(f"Debug: Event type: {event_type}")
-    print(f"Debug: Bucket: {bucket}")
-    print(f"Debug: File: {name}")
-    process_image(bucket, name)
+    if debugx:
+        print(f"DEBUGX:{eid}: Event ID: {event_id}")
+        print(f"DEBUGX:{eid}: Event type: {event_type}")
+        print(f"DEBUGX:{eid}: Bucket: {bucket}")
+        print(f"DEBUGX:{eid}: File: {name}")
+    process_image(bucket, name, eid)
 
 # Get image content and extract text using document AI
 
 
-def process_image(bucket_name: str, file_name: str):
+def process_image(bucket_name: str, file_name: str, eid: str):
+    debugx = False
+    if os.environ.get('DEBUGX') == "1":
+        debugx = True
+
     project_no = os.environ.get('PROJECT_NO')
     proc_location = os.environ.get('PROC_LOCATION')
     proc_id = os.environ.get('PROC_ID')
@@ -46,7 +58,8 @@ def process_image(bucket_name: str, file_name: str):
 
     proc_name = client.processor_path(project_no, proc_location, proc_id)
 
-    print(f"Debug: Processor name: {proc_name}")
+    if debugx:
+        print(f"DEBUGX:{eid}: Processor name: {proc_name}")
 
     raw_document = documentai.RawDocument(content=image_content, mime_type=mime_type)
 
@@ -55,22 +68,33 @@ def process_image(bucket_name: str, file_name: str):
     result = client.process_document(request=request)
 
     extracted_text = result.document
-    publish_text(bucket_name, file_name, extracted_text.text)
+    publish_text(bucket_name, file_name, extracted_text.text, eid)
 
 
 # Publish image metadata and extracted text to pub/sub
 
 
-def publish_text(bucket_name: str, file_name: str, extracted_text):
+def publish_text(bucket_name: str, file_name: str, extracted_text, eid: str):
+    debugx = False
+    if os.environ.get('DEBUGX') == "1":
+        debugx = True
+
     project_id = os.environ.get('PROJECT_ID')
     topic_id = os.environ.get('TOPIC_ID')
 
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(project_id, topic_id)
 
-    print(f"Debug: Topic path: {topic_path}")
+    if debugx:
+        print(f"DEBUGX:{eid}: Topic path: {topic_path}")
 
-    data_str = f"bucket:{bucket_name},name:{file_name},text:{extracted_text}"
-    data = data_str.encode("utf-8")
+    data = {}
+    data['bucket'] = bucket_name
+    data['file_name'] = file_name
+    data['extracted_text'] = extracted_text
+
+    json_data = json.dumps(data)
+    data = json_data.encode("utf-8")
     future = publisher.publish(topic_path, data)
-    print(f"Debug: {future.result()}")
+    if debugx:
+        print(f"DEBUGX:{eid}: {future.result()}")
