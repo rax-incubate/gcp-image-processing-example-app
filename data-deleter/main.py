dc43bb08b68ce6ebc795dd526fb3c587fd5b3bc7
srirajan <sriram.rajan@gmail.com>
2023-02-07 12:12:38 +0000

    added retries

--- data-deleter/main.py
+++ data-deleter/main.py
@@ -2,10 +2,11 @@ import functions_framework
 import os
 import json
 import uuid
+import time
 from google.cloud import storage
 from google.api_core.client_options import ClientOptions
 from google.cloud import bigquery
-
+from random import randrange
 
 @functions_framework.cloud_event
 # receive delete image events from GCS
@@ -34,6 +35,7 @@ def delete_data(bucket, filename, eid):
     if os.environ.get('DEBUGX') == "1":
         debugx = True
 
+    project_id = os.environ.get('PROJECT_ID')
     bq_dataset_id = os.environ.get('BQ_DATASET_ID')
     bq_table_id = os.environ.get('BQ_TABLE_ID')
 
@@ -43,6 +45,7 @@ def delete_data(bucket, filename, eid):
     client = bigquery.Client()
 
     sql = list()
+    data_entry_type = "delete"
     sql.append("DELETE FROM " + bq_dataset_id + "." + bq_table_id)
     sql.append(" WHERE bucket='" + bucket + "'")
     sql.append(" AND filename='" + filename + "'")
@@ -51,13 +54,21 @@ def delete_data(bucket, filename, eid):
     if debugx:
         print(f"DEBUGX:" + eid + ":" + "Query:" + query)
 
-
     if query:
-        query_job = client.query(query)
-
         if query_job.errors:
-            print(f"DEBUGX:" + eid + ":" + "Error in executing query:" + query_job.errors)
+            print(f"DEBUGX:" + eid + ":" + "Error in executing query:")
+
+            retries = 10
+            while retries > 0 and query_job.errors:
+                rand_sleep = (10/retries) + randrange(5)
+                print(f"DEBUGX:" + eid + ":" + "Sleeping " + str(rand_sleep) + " seconds and retrying query " + str(retries) )
+                time.sleep(rand_sleep)
+                query_job = client.query(query)
+                retries = retries - 1
+        if query_job.errors:
+            print(f"DEBUGX:{eid}:bucket={bucket},filename={filename},type={data_entry_type},Result=Max retries reached")
+        else:
+            print(f"DEBUGX:{eid}:bucket={bucket},filename={filename},type={data_entry_type},Result=Data entry succeeded")
 
     else:
         print(f"DEBUGX:" + eid + ":" + "Empty Query")
-
